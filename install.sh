529781cf0faeef9c4e18acf37979ef8adf7abf5c
codementum <ltharri1@uncc.edu>
2013-03-12 13:58:21 -0400

    install wasn't merged

--- install.sh
+++ install.sh
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-ln -s vimrc ~/.vimrc 
-ln -s zshrc ~/.zshrc 
-ln -s tmux.conf ~/.tmux.conf 
-
-# git setup
-git config --global user.name "Lane Harrison" 
-git config --global user.email lanetharrison@gmail.com
-=======
 # remove old, link new
 cd ~
 rm .vimrc .zshrc .tmux.conf
@@ -22,4 +13,7 @@ git submodule update
 # copy zsh theme to oh-my-zsh
 cd zsh/oh-my-zsh/themes
 ln -s ../../themes/knuth.zsh-theme knuth.zsh-theme
->>>>>>> 5a21df03f353cd511ab8c17a1ab7fc2d71a55f02
+
+# git setup
+git config --global user.name "Lane Harrison" 
+git config --global user.email lanetharrison@gmail.com
